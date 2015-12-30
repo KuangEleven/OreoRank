737b729d2caa2bd42803548003489a1de55e76bd
Bay Grabowski <bgrabowski@telesign.com>
2015-12-29 17:03:49 -0800

    Inital Django code

---
+++ oreoranksite/oreorankapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Cookie, CookieScore
+
+admin.site.register(Cookie)
+admin.site.register(CookieScore)
---
+++ oreoranksite/oreorankapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Cookie(models.Model):
+    def __str__(self):
+       return self.name
+    name = models.CharField(max_length=200)
+
+class CookieScore(models.Model):
+    def __str__(self):
+       return self.user.name + ' ' + self.cookie.name
+    cookie = models.ForeignKey(Cookie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.FloatField()
